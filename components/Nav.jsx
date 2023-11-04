2c8021f9becc3a02839f050f444455579867ab32
wesleysan7os <wesley.santos.comp@gmail.com>
2023-11-04 13:35:10 -0300

    feat: basic project structure

---
+++ components/Nav.jsx
@@ -0,0 +1,42 @@
+"use client";
+
+import Link from 'next/link';
+import Image from 'next/image';
+import { signIn, signOut, useSession, getProviders } from 'next-auth/react';
+import { useStaste, useEffect } from 'react';
+
+import React from 'react'
+
+const Nav = () => {
+	const isUserLoggedIn = true;
+
+  return (
+    <nav className="flex-between w-full mb-16 pt-3">
+        <Link href="/" className="flex gap-2 flex-center">
+            <Image 
+                src="assets/images/logo.svg"
+                alt="Promptopia Logo"
+                width={30}
+                height={30}
+                className="object-contain"
+            />
+            <p className="logo_text">Promptopia</p>
+        </Link>
+
+        {/* Mobile Navigation */}
+        <div className="sm:flex hidden">
+            {isUserLoggedIn ? (
+							<div className="flex gap-3 md:gap-5">
+								<Link href="/create-prompt" className="black_btn">
+									Create Post
+								</Link>
+							</div>
+						): (
+							<></>
+						)}
+        </div>
+    </nav>
+  )
+}
+
+export default Nav
