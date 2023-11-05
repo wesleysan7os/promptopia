231bbe0281c50a486d9644f951a40ba9c958475e
wesleysan7os <wesley.santos.comp@gmail.com>
2023-11-05 18:03:01 -0300

    feat: create atlas user collection

--- app/layout.jsx
+++ app/layout.jsx
@@ -4,8 +4,8 @@ import Nav from '@components/Nav';
 import Provider from '@components/Provider';
 
 export const metadata = {
-    title: "Promptopia",
-    description: "Discover & Share AI Prompts"
+  title: "Promptopia",
+  description: "Discover & Share AI Prompts"
 }
 
 const RootLayout = ({ children }) => {
--- utils/database.js
+++ utils/database.js
@@ -13,13 +13,11 @@ export const connectToDB = async () => {
   try {
     await mongoose.connect(process.env.MONGODB_URI, {
       dbName: 'share_prompt',
-      useNewUrlParser: true,
-      useUnifiedTopology: true
     });
 
     isConnected = true;
     console.log('MongoDB connected');
-    
+
   } catch (error) {
     console.log(error);
   }
