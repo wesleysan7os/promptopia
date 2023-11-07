d4e225a755fe8b4fb1f9c9eb46dd02fc355b8d59
wesleysan7os <wesley.santos.comp@gmail.com>
2023-11-07 17:59:29 -0300

    feat: profile & update-prompt pages

--- components/PromptCard.jsx
+++ components/PromptCard.jsx
@@ -8,6 +8,9 @@ import { usePathname, useRouter } from 'next/navigation';
 
 const PromptCard = ({ post, handleTagClick, handleEdit, handleDelete }) => {
   const [copied, setCopied] = useState('');
+  const { data: session } = useSession();
+  const pathName = usePathname();
+  const router = useRouter();
 
    const handleCopy = () => {
     setCopied(post.prompt);
@@ -43,6 +46,7 @@ const PromptCard = ({ post, handleTagClick, handleEdit, handleDelete }) => {
               ? '/assets/icons/tick.svg'
               : '/assets/icons/copy.svg'
             }
+            alt="copy_icon"
             width={12}
             height={12}
           />
@@ -54,6 +58,23 @@ const PromptCard = ({ post, handleTagClick, handleEdit, handleDelete }) => {
         onClick={() => handleTagClick && handleTagClick(post.tag)}
       >{post.prompt}</p>
       <p>{post.tag}</p>
+
+      {session?.user.id === post.creator._id && pathName === '/profile' && (
+        <div className="mt-5 flex-center gap-4 border-t border-gray-100 pt-3">
+          <p 
+            className="font-inter text-sm green_gradient cursor-pointer"
+            onClick={handleEdit}
+          >
+            Edit
+          </p>
+          <p 
+            className="font-inter text-sm green_gradient cursor-pointer"
+            onClick={handleDelete}
+          >
+            Delete
+          </p>
+        </div>
+      )}
     </div>
   )
 }
