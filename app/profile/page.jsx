1c09f9bb445c8622f23ef3a771ef34dae1725128
wesleysan7os <wesley.santos.comp@gmail.com>
2023-11-08 12:52:37 -0300

    feat: delete prompt

--- app/profile/page.jsx
+++ app/profile/page.jsx
@@ -26,7 +26,18 @@ const MyProfile = () => {
   }
 
   const handleDelete = async (post) => {
+    const hasConfirmed = confirm("Are you sure you want to delete this prompt?");
 
+    if (hasConfirmed) {
+      try {
+        await fetch(`/api/prompt/${post._id.toString()}`, { method: 'DELETE'});
+        const filteredPosts = posts.filter((p) => p._id !== post._id);
+        setPosts(filteredPosts);
+
+      } catch (error) {
+        console.log(error);
+      }
+    }
   }
 
   return (
@@ -35,7 +46,7 @@ const MyProfile = () => {
       desc="Welcome to your personalized profile page"
       data={posts}
       handleEdit={handleEdit}
-      handleDelete={() => {}}
+      handleDelete={handleDelete}
     />
   )
 }
